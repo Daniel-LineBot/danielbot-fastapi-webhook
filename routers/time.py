f2744083ad0ae3e7f6bd541abad6a3ea0fb7d168
Daniel-LineBot <d651112@gmail.com>
2025-07-18 15:06:46 +0800

    Update time.py

--- routers/time.py
+++ routers/time.py
@@ -4,3 +4,7 @@ def get_tw_time():
     """回傳台灣時區的 datetime.now()"""
     taiwan_tz = timezone(timedelta(hours=8))
     return datetime.now(taiwan_tz)
+
+def get_tw_time_str(fmt="%Y%m%d"):
+    """回傳指定格式的台灣時間字串"""
+    return get_tw_time().strftime(fmt)
