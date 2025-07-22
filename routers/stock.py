a44ca18e5c97110f3b086faa6f7e771a1ed8dee3
Daniel-LineBot <d651112@gmail.com>
2025-07-22 11:39:58 +0800

    Update stock.py

--- routers/stock.py
+++ routers/stock.py
@@ -13,10 +13,9 @@ import requests
 from bs4 import BeautifulSoup
 from routers.time import get_tw_time, get_tw_time_str, is_market_open, twse_open_range  # ✅ 引入時間模組 #20250718 added.
 from routers.time import twse_status, get_tw_time_str #20250718 added.
-#from routers.name import get_stock_name  #20250721 added.
 from routers.goodinfo import get_goodinfo_price_robust #20250722 added.
 from routers.goodinfo import get_yahoo_price
-
+#from routers.name import get_stock_name_industry
 #20250718_v2
 
 
