bcf1c10750c88fae20c45656f0ba799ba43cfc4c
Daniel-LineBot <d651112@gmail.com>
2025-07-25 08:52:43 +0800

    Update log_trace_decorator.py

--- webhook/log_trace_decorator.py
+++ webhook/log_trace_decorator.py
@@ -2,6 +2,22 @@
 import logging
 logger = logging.getLogger("uvicorn")
 
+import inspect
+
+def log_trace(tag: str):
+    def decorator(fn):
+        async def wrapper(*args, **kwargs):
+            logger.info(f"🟡 START ➜ {tag}")
+            if inspect.iscoroutinefunction(fn):
+                result = await fn(*args, **kwargs)
+            else:
+                result = fn(*args, **kwargs)
+            logger.info(f"🟢 END ➜ {tag}")
+            return result
+        return wrapper
+    return decorator
+
+"""
 def log_trace(tag: str):
     def decorator(fn):
         async def wrapper(*args, **kwargs):
@@ -11,3 +27,4 @@ def log_trace(tag: str):
             return result
         return wrapper
     return decorator
+"""
