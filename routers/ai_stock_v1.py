71ae85f2f2d868bb3c9828f485741538814e44f0
Daniel-LineBot <d651112@gmail.com>
2025-07-25 12:16:18 +0800

    Update ai_stock_v1.py

--- routers/ai_stock_v1.py
+++ routers/ai_stock_v1.py
@@ -1,8 +1,8 @@
 # ai_stock_v1.py
 
 from routers.twse import get_twse_data
-from routers.publicinfo import get_mops_metadata
-from routers.goodinfo import get_goodinfo_metadata
+#from routers.publicinfo import get_mops_metadata
+#from routers.goodinfo import get_goodinfo_metadata
 import asyncio
 
 async def get_stock_info(stock_id: str, date: str = None) -> dict:
