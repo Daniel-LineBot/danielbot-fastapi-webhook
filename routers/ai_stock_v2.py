5c8e2b98a8fc7baa450420750e23c57470a1c848
Daniel-LineBot <d651112@gmail.com>
2025-07-29 08:54:31 +0800

    Update ai_stock_v2.py

--- routers/ai_stock_v2.py
+++ routers/ai_stock_v2.py
@@ -1,9 +1,9 @@
 from routers.ai_twse_v1 import get_twse_price, get_twse_dividend    
-from routers.ai_finmind_v1 import get_finmind_price, get_finmind_dividend
+#from routers.ai_finmind_v1 import get_finmind_price, get_finmind_dividend
 
 async def get_stock_info(stock_id: str, date: str = None) -> dict:
     try:
-        data = await get_finmind_price(stock_id, date)
+     #   data = await get_finmind_price(stock_id, date)
         if not data or "error" in data:
             data = await get_twse_price(stock_id, date)
         return data
@@ -12,7 +12,7 @@ async def get_stock_info(stock_id: str, date: str = None) -> dict:
 
 async def get_dividend_info(stock_id: str) -> dict:
     try:
-        data = await get_finmind_dividend(stock_id)
+     #   data = await get_finmind_dividend(stock_id)
         if not data or "error" in data:
             data = await get_twse_dividend(stock_id)
         return data
