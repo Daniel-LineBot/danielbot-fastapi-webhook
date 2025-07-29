6ddc13489345410e68604b9fb1c1f42eea3eb49b
Daniel-LineBot <d651112@gmail.com>
2025-07-29 15:45:02 +0800

    Update ai_stock_v2.py

--- routers/ai_stock_v2.py
+++ routers/ai_stock_v2.py
@@ -3,6 +3,8 @@ from routers.ai_twse_v1 import get_twse_price, get_twse_dividend
 from routers.ai_finmind_v1 import get_finmind_price, get_finmind_dividend
 from modules.tdcc_client import get_cdib_dividend
 from modules.dividend_reply import get_dividend_info
+from loguru import logger  # ✅ 必要補上這行
+
 
 
 async def get_stock_info(stock_id: str, date: str = None) -> dict:
