fb8a17ac80b25492c6b5eb6386756c6d82b13efe
Daniel-LineBot <d651112@gmail.com>
2025-08-01 17:00:46 +0800

    Add files via upload

--- modules/dividend_fetcher.py
+++ modules/dividend_fetcher.py
@@ -1,15 +1,42 @@
+
 async def fetch_twse_dividend(stock_id, year=None):
-    # TODO: 串接TWSE API
-    # 範例假資料
-    return {"source": "TWSE", "data": f"{stock_id} {year or ''} TWSE配息資料"}
+    # TODO: 串接 TWSE API
+    return {
+        "source": "TWSE",
+        "data": {
+            "year": year or 2024,
+            "cash_dividend": "1.5",
+            "stock_dividend": "0.0",
+            "ex_dividend_date": "2024-07-01",
+            "source": "TWSE"
+        }
+    }
 
 async def fetch_finmind_dividend(stock_id, year=None):
-    # TODO: 串接FinMind API
-    return {"source": "FinMind", "data": f"{stock_id} {year or ''} FinMind配息資料"}
+    # TODO: 串接 FinMind API
+    return {
+        "source": "FinMind",
+        "data": {
+            "year": year or 2024,
+            "cash_dividend": "1.7",
+            "stock_dividend": "0.0",
+            "ex_dividend_date": "2024-06-28",
+            "source": "FinMind"
+        }
+    }
 
 async def fetch_central_depository_dividend(stock_id, year=None):
-    # TODO: 串接集保API
-    return {"source": "CentralDepository", "data": f"{stock_id} {year or ''} 集保配息資料"}
+    # TODO: 串接 集保 API
+    return {
+        "source": "TDCC",
+        "data": {
+            "year": year or 2024,
+            "cash_dividend": "1.6",
+            "stock_dividend": "0.0",
+            "ex_dividend_date": "2024-06-30",
+            "source": "TDCC"
+        }
+    }
 
 async def fetch_all_dividend(stock_id, year=None):
     result = []
